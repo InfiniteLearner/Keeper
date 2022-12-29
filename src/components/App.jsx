5e47fd1d073d0b061209e1078df7f3f202669977
InfiniteLearner <cyprien.guenard.pro@protonmail.com>
2022-12-29 12:04:24 +0100

    Add notes and props for Note component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,26 @@
 import Header from './Header' ;
 import Footer from './Footer' ;
 import Note from './Note' ;
+import notes from '../notes' ;
+
+function createNote(note){
+  return(
+    <Note key={note.key} title={note.title} content={note.content} />
+  );
+}
+
+console.log(notes);
+
+var arr = [1,2,3];
+
+console.log(arr);
+
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map(createNote)}
       <Footer />
     </div>
   );
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,13 +1,10 @@
 import React from "react" ;
 
-const title = "This is the note title" ;
-const content = "This is the note content" ;
-
-function Note(){
+function Note(props){
     return (
         <div className="note">
-            <h1>{title}</h1>
-            <p>{content}</p>
+            <h1>{props.title}</h1>
+            <p>{props.content}</p>
         </div>
     )
 }
